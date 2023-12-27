e0a85267087483a17fcb8c1ec9f017d55dfb9e7f
Augusto Ravazoli <augustoravazoli@gmail.com>
2023-12-27 08:50:19 -0300

    initial commit

---
+++ src/test/java/io/github/augustoravazoli/inventorymanagementsystem/ApplicationTests.java
@@ -0,0 +1,16 @@
+package io.github.augustoravazoli.inventorymanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@SpringBootTest
+@Import(TestApplication.class)
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
