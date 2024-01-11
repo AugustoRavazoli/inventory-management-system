beaa21f7277a4893efe9def3ffca87bbbce4901e
Augusto Ravazoli <augustoravazoli@gmail.com>
2024-01-11 07:45:30 -0300

    chore: drop tables on application restart in local profile

--- src/main/resources/database/data.sql
+++ src/main/resources/database/data.sql
@@ -6,7 +6,7 @@ INSERT INTO category (name) VALUES
     ('E'),
     ('F'),
     ('G'),
-    ('H') ON CONFLICT (name) DO NOTHING;
+    ('H');
 
 INSERT INTO product (name, category_id, quantity, price) VALUES
     ('A', 1, 50, '100'),
@@ -28,7 +28,7 @@ INSERT INTO product (name, category_id, quantity, price) VALUES
     ('Q', 1, 850, '900'),
     ('R', 2, 900, '950'),
     ('S', 3, 950, '1000'),
-    ('T', 4, 1000, '1050') ON CONFLICT (name) DO NOTHING;
+    ('T', 4, 1000, '1050');
 
 INSERT INTO customer (name, address, phone) VALUES
     ('A', 'A', 'A'),
@@ -38,32 +38,32 @@ INSERT INTO customer (name, address, phone) VALUES
     ('E', 'E', 'E'),
     ('F', 'F', 'F'),
     ('G', 'G', 'G'),
-    ('H', 'H', 'H') ON CONFLICT (name) DO NOTHING;
+    ('H', 'H', 'H');
 
-INSERT INTO "order" (id, status, customer_id) VALUES
-    (1, 'UNPAID', 1),
-    (2, 'UNPAID', 2),
-    (3, 'UNPAID', 3),
-    (4, 'UNPAID', 4),
-    (5, 'PAID', 5),
-    (6, 'PAID', 6) ON CONFLICT (id) DO NOTHING;
+INSERT INTO "order" (status, customer_id) VALUES
+    ('UNPAID', 1),
+    ('UNPAID', 2),
+    ('UNPAID', 3),
+    ('UNPAID', 4),
+    ('PAID', 5),
+    ('PAID', 6);
 
-INSERT INTO order_item (id, quantity, product_id, order_id) VALUES
-    (1, 1, 1, 1),
-    (2, 2, 2, 1),
-    (3, 3, 3, 1),
-    (4, 1, 1, 2),
-    (5, 2, 2, 2),
-    (6, 3, 3, 2),
-    (7, 1, 1, 3),
-    (8, 2, 2, 3),
-    (9, 3, 3, 3),
-    (10, 1, 1, 4),
-    (11, 2, 2, 4),
-    (12, 3, 3, 4),
-    (13, 1, 1, 5),
-    (14, 2, 2, 5),
-    (15, 3, 3, 5),
-    (16, 1, 1, 6),
-    (17, 2, 2, 6),
-    (18, 3, 3, 6) ON CONFLICT (id) DO NOTHING;
+INSERT INTO order_item (quantity, product_id, order_id) VALUES
+    (1, 1, 1),
+    (2, 2, 1),
+    (3, 3, 1),
+    (1, 1, 2),
+    (2, 2, 2),
+    (3, 3, 2),
+    (1, 1, 3),
+    (2, 2, 3),
+    (3, 3, 3),
+    (1, 1, 4),
+    (2, 2, 4),
+    (3, 3, 4),
+    (1, 1, 5),
+    (2, 2, 5),
+    (3, 3, 5),
+    (1, 1, 6),
+    (2, 2, 6),
+    (3, 3, 6);
--- src/main/resources/database/schema.sql
+++ src/main/resources/database/schema.sql
@@ -1,9 +1,15 @@
-CREATE TABLE IF NOT EXISTS category (
+DROP TABLE IF EXISTS order_item;
+DROP TABLE IF EXISTS "order";
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS customer;
+
+CREATE TABLE category (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS product (
+CREATE TABLE product (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     category_id BIGINT REFERENCES category(id) ON DELETE SET NULL,
@@ -11,21 +17,21 @@ CREATE TABLE IF NOT EXISTS product (
     price DECIMAL(12, 2) NOT NULL CHECK (price >= 0.01)
 );
 
-CREATE TABLE IF NOT EXISTS customer (
+CREATE TABLE customer (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     address VARCHAR(255) NOT NULL,
     phone VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "order" (
+CREATE TABLE "order" (
     id BIGSERIAL PRIMARY KEY,
     status VARCHAR(255) NOT NULL CHECK (status IN ('UNPAID', 'PAID')),
     "date" date NOT NULL DEFAULT CURRENT_DATE,
     customer_id BIGINT NOT NULL REFERENCES customer(id)
 );
 
-CREATE TABLE IF NOT EXISTS order_item (
+CREATE TABLE order_item (
     id BIGSERIAL PRIMARY KEY,
     quantity INTEGER NOT NULL CHECK (quantity > 0),
     product_id BIGINT NOT NULL REFERENCES product(id),
